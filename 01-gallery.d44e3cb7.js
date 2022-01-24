07f38c22e6fab1381161a3af56aff059dc114e5b
rubtsov86 <rubtsov1986@bigmir.net>
2022-01-24 19:35:25 +0000

    Deploying to gh-pages from @ rubtsov86/goit-js-hw-08@d7ec794d2916f5e773edad7bd1e9097505bd736d 🚀

--- 01-gallery.d44e3cb7.js
+++ 01-gallery.d44e3cb7.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),t=i(require("simplelightbox"));function i(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const l=document.querySelector(".gallery");function r({preview:e,original:t,description:i}){return`<a class="gallery__item" href="${t}">\n  <img class="gallery__image" src="${e}" alt="${i}" />\n</a>`}const a=e.galleryItems.map(e=>r(e)).join("");l.insertAdjacentHTML("beforeend",a);const n=new t.default(".gallery a");n.options.captionsData="alt",n.options.captionDelay=250;
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel/01-gallery.d44e3cb7.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.d44e3cb7.js.map
--- 02-video.fbe2952c.js
+++ 02-video.fbe2952c.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),n=new Vimeo.Player(o);n.setCurrentTime(localStorage.getItem("videoplayer-current-time"));const i=function(e){n.getCurrentTime().then(function(e){localStorage.setItem("videoplayer-current-time",e),console.log(localStorage.getItem("videoplayer-current-time"))}).catch(function(e){})};n.on("timeupdate",(0,t.default)(i,1e3));
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/parcel/02-video.fbe2952c.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.fbe2952c.js.map
